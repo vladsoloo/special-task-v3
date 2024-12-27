af3c43e922030b2bf2ea7907abe1136efd622adc
Vlad Solo <solo.vlad55555@icloud.com>
2024-12-27 16:10:42 +0300

    сделал

--- task2.py
+++ task2.py
@@ -1,15 +1,14 @@
-A = True
-B = False
-C = False
+# Исходные значения
+X = False
+Y = True
+Z = False
 
-# а) не А и B
-result_a = not A and B
-print(f"а) {result_a}")
+# Вычисление значений логических выражений
+a = X or Z
+b = X and Y
+c = X and Z
 
-# б) А или не B
-result_b = A or not B
-print(f"б) {result_b}")
-
-# в) А и B или C
-result_c = (A and B) or C
-print(f"в) {result_c}")
+# Вывод результатов
+print("а:", a)
+print("б:", b)
+print("в:", c)
